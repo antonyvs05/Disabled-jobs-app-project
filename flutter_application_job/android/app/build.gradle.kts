d5f506e5755bea299d1750248411d14f27a5ef9b
antonyvs05 <vanstratenantony@gmail.com>
2026-02-08 01:24:32 +0200

    functional navigation and build

--- flutter_application_job/android/app/build.gradle.kts
+++ flutter_application_job/android/app/build.gradle.kts
@@ -21,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_application_job"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,10 +30,20 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+    }
+
+    lint {
+        abortOnError = false
+        disable.add("KotlinNullnessAnnotation")
+        disable.add("PropertyEscape")
     }
 }
 
