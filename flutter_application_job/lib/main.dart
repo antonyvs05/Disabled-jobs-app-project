e774e6129d687d912750bb9926fa81f29a96e3c1
antonyvs05 <vanstratenantony@gmail.com>
2026-02-10 13:09:01 +0200

    defualt files for mac

--- flutter_application_job/lib/main.dart
+++ flutter_application_job/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'signup.dart';
-import 'home.dart';
 import 'profile.dart';
 
 void main() async {
